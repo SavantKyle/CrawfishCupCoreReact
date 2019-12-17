2f530c2dd9333723439b8331a8a17e71d71251b2
Kyle Savant <kyle.savant@outlook.com>
2019-12-16 20:32:16 -0600

    Initial Commit

---
+++ DataProvider.Contracts/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DataProvider.Contracts
+{
+    public class Class1
+    {
+    }
+}
---
+++ Services.SharedContracts/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Services.SharedContracts
+{
+    public class Class1
+    {
+    }
+}
